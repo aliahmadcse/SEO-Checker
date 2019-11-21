4edaf599a54d5c23918da56cfa7817467b4136c8
aliahmadcse <aliahmadcse@gmail.com>
2019-11-22 01:04:42 +0500

    Starts reading file

--- js/app.js
+++ js/app.js
@@ -3,19 +3,32 @@ const chooseButton = document.querySelector(".selectBtn");
 const list = document.querySelector(".warningList");
 const choosenMessage = document.querySelector(".choosenMessage");
 chooseButton.addEventListener("click", chooseFile);
-inputFileButton.addEventListener("change", getName);
+inputFileButton.addEventListener("change", readFile);
 
 function chooseFile() {
   inputFileButton.click();
 }
 
-function getName() {
+function readFile() {
   // returns list of selected files
   const file = this.files;
-  // console.log(file);
+  // console.log(file[0]);
   if (file.length == 0) {
     choosenMessage.innerHTML = "No file choosen";
+    return;
   } else {
     choosenMessage.innerHTML = "Click analyze to test the file";
   }
+  const reader = new FileReader();
+  reader.readAsText(file[0]);
+  reader.addEventListener("loadstart", function() {
+    console.log("Reading started");
+  });
+  reader.addEventListener("load", analyzeFile);
+  reader.readAsText(file);
+}
+
+function analyzeFile(e) {
+  const text = e.target.result;
+  console.log(text);
 }
