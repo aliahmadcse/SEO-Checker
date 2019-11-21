443cbd18627c39bc162b0f7350f0df769d57fdfe
aliahmadcse <aliahmadcse@gmail.com>
2019-11-21 15:30:13 +0500

    Adds list dynamically for warnings

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-const chooseButton = document.querySelector("#chooseButton");
+const chooseButton = document.querySelector(".selectBtn");
+const list = document.querySelector(".warningList");
+chooseButton.addEventListener("click", render);
+
+function render() {
+  for (let i = 0; i < 10; i++) {
+    const listItem = document.createElement("li");
+    listItem.textContent = "Hello Earth";
+    list.appendChild(listItem);
+  }
+}
