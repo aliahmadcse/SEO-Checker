eccca6dfd282aae2ccceefcc3cd9aa3897002829
aliahmadcse <aliahmadcse@gmail.com>
2019-11-23 20:49:46 +0500

    adds the script src to the read file

--- js/app.js
+++ js/app.js
@@ -1,34 +1,50 @@
-const inputFileButton = document.querySelector(".inputFile");
-const chooseButton = document.querySelector(".selectBtn");
+const inputFileBtn = document.querySelector(".inputFile");
+const selectBtn = document.querySelector(".selectBtn");
 const list = document.querySelector(".warningList");
-const choosenMessage = document.querySelector(".choosenMessage");
-chooseButton.addEventListener("click", chooseFile);
-inputFileButton.addEventListener("change", readFile);
+const selectMessage = document.querySelector(".selectMessage");
 
-function chooseFile() {
-  inputFileButton.click();
-}
+selectBtn.addEventListener("click", function() {
+  inputFileBtn.click();
+});
+
+inputFileBtn.addEventListener("change", readFile);
 
 function readFile() {
   // returns list of selected files
   const file = this.files;
-  // console.log(file[0]);
+  //validation
   if (file.length == 0) {
-    choosenMessage.innerHTML = "No file choosen";
+    selectMessage.innerHTML = "No file choosen";
     return;
   } else {
-    choosenMessage.innerHTML = "Click analyze to test the file";
+    selectMessage.innerHTML = "Click analyze to test the file";
   }
+  //FileReader api
   const reader = new FileReader();
+  //reading the first file as text
   reader.readAsText(file[0]);
-  reader.addEventListener("loadstart", function() {
-    console.log("Reading started");
-  });
-  reader.addEventListener("load", analyzeFile);
-  reader.readAsText(file);
+  //event is triggered when reader encounter some error
+  reader.onerror = function() {
+    console.log("Some Error in Reading file");
+    reader.abort();
+  };
+  //event triggered, when reader finishes reading file
+  reader.onload = analyzeFile;
 }
 
 function analyzeFile(e) {
-  const text = e.target.result;
-  console.log(text);
+  //getting the file text as string
+  const fileText = e.target.result;
+  const len = fileText.length;
+  //the string to be concatenated with the file text
+  const jsStr = '<script src="../js/analyze.js"></script>\n';
+  const searchItem = "</body>";
+  //finding the index of </body> tag in fileText
+  const indexOfBody = fileText.indexOf(searchItem);
+  //extraxting the string before </body>
+  const strBeforeBody = fileText.slice(0, indexOfBody);
+  //extracting the remaining string
+  const remainingStr = fileText.slice(indexOfBody);
+  //concatenating the three strings
+  const finalText = strBeforeBody.concat(jsStr, remainingStr);
 }
